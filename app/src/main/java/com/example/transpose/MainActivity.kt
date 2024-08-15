c1ea2282cb3f241880b4626f06e48f7a4d0c6d4d
joh <joh99111@gmail.com>
2024-08-15 17:37:32 +0900

    Feat: Feature for Navigation Management

--- app/src/main/java/com/example/transpose/MainActivity.kt
+++ app/src/main/java/com/example/transpose/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
@@ -27,9 +28,14 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.transpose.ui.components.appbar.MainAppBar
+import com.example.transpose.ui.components.bottomnavigation.BottomNavigationBar
+import com.example.transpose.ui.screen.convert.ConvertMainScreen
+import com.example.transpose.ui.screen.home.HomeMainScreen
 import com.example.transpose.ui.screen.home.HomeViewModel
 import com.example.transpose.ui.screen.home.homeplaylist.HomePlaylistScreen
+import com.example.transpose.ui.screen.library.LibraryMainScreen
 import com.example.transpose.ui.theme.TransposeTheme
+import com.example.transpose.utils.LogComposableLifecycle
 import com.example.transpose.utils.Logger
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -55,26 +61,41 @@ class MainActivity : ComponentActivity() {
         mainViewModel: MainViewModel
     ) {
 
-
         val homeViewModel: HomeViewModel by viewModels()
-
+        val navigationViewModel: NavigationViewModel by viewModels()
 
         val searchWidgetState by mainViewModel.searchWidgetState.collectAsState()
         val searchTextState by mainViewModel.searchTextState.collectAsState()
         val suggestionKeywords by mainViewModel.suggestionKeywords.collectAsState()
         val isSearchBarActive by mainViewModel.isSearchBarActive.collectAsState()
-
-
         val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior(rememberTopAppBarState())
+
+
         val navController = rememberNavController()
+        val mainCurrentRoute by navigationViewModel.mainNavCurrentRoute.collectAsState()
 
         val nestedScrollConnection = remember {
             object : NestedScrollConnection {
                 override fun onPreScroll(available: Offset, source: NestedScrollSource): Offset {
-                    // scrollBehavior의 nestedScrollConnection도 호출
                     val behaviorConsumed = scrollBehavior.nestedScrollConnection.onPreScroll(available, source)
                     return behaviorConsumed
                 }
+            }
+        }
+        LogComposableLifecycle(screenName = "MainScreen")
+
+
+        LaunchedEffect(mainCurrentRoute) {
+            if (mainCurrentRoute != navigationViewModel.mainNavPreviousRoute) {
+                navController.navigate(mainCurrentRoute) {
+                    // 백 스택 관리
+                    popUpTo(navController.graph.startDestinationId) {
+                        saveState = true
+                    }
+                    launchSingleTop = true
+                    restoreState = true
+                }
+                navigationViewModel.mainNavPreviousRoute = mainCurrentRoute
             }
         }
 
@@ -99,22 +120,30 @@ class MainActivity : ComponentActivity() {
                     onSearchBarActiveChanged = {mainViewModel.updateIsSearchBarExpanded(it)},
                     scrollBehavior = scrollBehavior
                 )
+            },
+            bottomBar = {
+                BottomNavigationBar(navigationViewModel = navigationViewModel)
             }
 
+
         ) { innerPadding ->
-            BackHandler() {
-                Logger.d("뒤로가기 $searchWidgetState")
-            }
+
             NavHost(
-                navController = navController, startDestination = "home", modifier = Modifier
+                navController = navController, startDestination = Route.Home.route, modifier = Modifier
                     .fillMaxSize()
                     .padding(innerPadding)
                     .nestedScroll(nestedScrollConnection)
 
             ) {
 
-                composable(route = "home") {
-                    HomePlaylistScreen(viewModel = homeViewModel)
+                composable(route = Route.Home.route) {
+                    HomeMainScreen(homeViewModel = homeViewModel)
+                }
+                composable(route = Route.Convert.route){
+                    ConvertMainScreen()
+                }
+                composable(route = Route.Library.route){
+                    LibraryMainScreen()
                 }
             }
 
