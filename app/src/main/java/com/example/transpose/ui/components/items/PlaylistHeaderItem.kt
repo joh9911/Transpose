ff5e0fcd480f342e9a1e4821849e61620b3f8e52
joh <joh99111@gmail.com>
2024-08-17 16:40:46 +0900

    Feat: Add PlaylistHeaderItem

---
+++ app/src/main/java/com/example/transpose/ui/components/items/PlaylistHeaderItem.kt
@@ -0,0 +1,61 @@
+package com.example.transpose.ui.components.items
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+import com.example.transpose.data.model.NewPipePlaylistData
+
+@Composable
+fun PlaylistHeaderItem(playlistData: NewPipePlaylistData?) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(bottom = 20.dp)
+    ) {
+        AsyncImage(
+            model = playlistData?.thumbnailUrl,
+            contentDescription = "Playlist Thumbnail",
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(200.dp)
+                .padding(top = 20.dp, start = 20.dp, end = 20.dp)
+                .clip(RoundedCornerShape(16.dp)),
+            contentScale = ContentScale.Crop
+        )
+
+        Text(
+            text = playlistData?.title ?: "",
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 20.dp),
+            textAlign = TextAlign.Center,
+            fontSize = 30.sp,
+            fontWeight = FontWeight.Bold
+        )
+
+        Text(
+            text = playlistData?.description ?: "",
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 10.dp),
+            textAlign = TextAlign.Center,
+            maxLines = 3,
+            overflow = TextOverflow.Ellipsis,
+            color = Color.Gray
+        )
+    }
+}
