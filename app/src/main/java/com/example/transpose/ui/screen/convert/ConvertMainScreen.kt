37527d3e680dc900445b69ca36bd94413e195781
joh <joh99111@gmail.com>
2024-08-17 16:35:57 +0900

    Feat: Add Navigation ViewModel Parameter

--- app/src/main/java/com/example/transpose/ui/screen/convert/ConvertMainScreen.kt
+++ app/src/main/java/com/example/transpose/ui/screen/convert/ConvertMainScreen.kt
@@ -4,10 +4,14 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import com.example.transpose.NavigationViewModel
 import com.example.transpose.utils.LogComposableLifecycle
 
 @Composable
-fun ConvertMainScreen(){
+fun ConvertMainScreen(
+    navigationViewModel: NavigationViewModel,
+    convertViewModel: ConvertViewModel
+){
     LogComposableLifecycle(screenName = "ConvertMainScreen")
 
     Box(
--- app/src/main/java/com/example/transpose/ui/screen/library/LibraryMainScreen.kt
+++ app/src/main/java/com/example/transpose/ui/screen/library/LibraryMainScreen.kt
@@ -4,10 +4,14 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import com.example.transpose.NavigationViewModel
 import com.example.transpose.utils.LogComposableLifecycle
 
 @Composable
-fun LibraryMainScreen(){
+fun LibraryMainScreen(
+    navigationViewModel: NavigationViewModel,
+    libraryViewModel: LibraryViewModel
+){
     LogComposableLifecycle(screenName = "LibraryMainScreen")
 
     Box(
