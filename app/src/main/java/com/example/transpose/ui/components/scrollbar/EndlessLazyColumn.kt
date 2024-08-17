7d66ee8552f058bb01756be34439f0ee05d79381
joh <joh99111@gmail.com>
2024-08-17 16:41:00 +0900

    Feat: Add EndlessLazyColumn

---
+++ app/src/main/java/com/example/transpose/ui/components/scrollbar/EndlessLazyColumn.kt
@@ -0,0 +1,61 @@
+package com.example.transpose.ui.components.scrollbar
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.transpose.ui.components.LoadingIndicator
+
+
+@Composable
+internal fun <H, T> EndlessLazyColumn(
+    modifier: Modifier = Modifier,
+    loading: Boolean = false,
+    listState: LazyListState = rememberLazyListState(),
+    hasMoreItems: Boolean,
+    items: List<T>,
+    headerData: H? = null,
+    itemKey: (T) -> Any,
+    headerContent: (@Composable (H) -> Unit)? = null,
+    itemContent: @Composable (T) -> Unit,
+    loadMore: () -> Unit
+) {
+
+    val reachedBottom: Boolean by remember { derivedStateOf { listState.reachedBottom() } }
+
+    // load more if scrolled to bottom
+    LaunchedEffect(reachedBottom) {
+        if (reachedBottom && !loading && hasMoreItems) loadMore()
+    }
+
+    LazyColumn(modifier = modifier, state = listState) {
+        if (headerData != null && headerContent != null) {
+            item {
+                headerContent(headerData)
+            }
+        }
+        items(items.size){ index ->
+            val item = items[index]
+            itemContent(item)
+            if (index == items.size - 1 && hasMoreItems)
+                LoadingIndicator()
+        }
+    }
+}
+
+
+private fun LazyListState.reachedBottom(): Boolean {
+    val lastVisibleItem = this.layoutInfo.visibleItemsInfo.lastOrNull()
+    return lastVisibleItem?.index != 0 && lastVisibleItem?.index == this.layoutInfo.totalItemsCount - 1
+}
