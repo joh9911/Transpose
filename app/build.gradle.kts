4cae04f95a07ccb3386bbee86ae115e14d777a23
joh <joh99111@gmail.com>
2024-08-20 23:17:45 +0900

    Feat: add ConstraintLayout

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -109,6 +109,8 @@ dependencies {
     implementation(libs.androidx.media3.exoplayer)
     implementation(libs.androidx.media3.exoplayer.dash)
     implementation(libs.androidx.media3.ui)
+
+    implementation (libs.androidx.constraintlayout.compose)
 }
 
 kapt {
