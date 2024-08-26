d16b1adb5b40fbc6e85b7167fdc916d34c157b21
joh9911 <joh9911@naver.com>
2024-08-26 23:12:27 +0900

    feat: Implement LibrarySearchResultScreen Address #5

--- app/src/main/java/com/example/transpose/ui/screen/library/search_result/LibrarySearchResultScreen.kt
+++ app/src/main/java/com/example/transpose/ui/screen/library/search_result/LibrarySearchResultScreen.kt
@@ -1,17 +1,90 @@
 package com.example.transpose.ui.screen.library.search_result
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
 import com.example.transpose.MainViewModel
 import com.example.transpose.MediaViewModel
+import com.example.transpose.data.model.newpipe.NewPipeContentListData
+import com.example.transpose.data.model.newpipe.NewPipeVideoData
+import com.example.transpose.navigation.Route
+import com.example.transpose.navigation.viewmodel.NavigationViewModel
+import com.example.transpose.ui.common.UiState
+import com.example.transpose.ui.components.items.CommonVideoItem
+import com.example.transpose.ui.components.items.LoadingIndicator
+import com.example.transpose.ui.components.scrollbar.EndlessLazyColumn
 import com.example.transpose.ui.screen.convert.search_result.ConvertSearchResultViewModel
+import com.example.transpose.ui.screen.home.search_result.HomeSearchResultViewModel
 
 @Composable
 fun LibrarySearchResultScreen(
     librarySearchResultViewModel: LibrarySearchResultViewModel,
     mainViewModel: MainViewModel,
     mediaViewModel: MediaViewModel,
+    navigationViewModel: NavigationViewModel,
     query: String?
 ){
-    Text("LibrarySearchResultScreen")
+
+    val searchResults by librarySearchResultViewModel.searchResults.collectAsState()
+    val uiState by librarySearchResultViewModel.searchUiState.collectAsState()
+    val isMoreItemsLoading by librarySearchResultViewModel.isMoreItemsLoading.collectAsState()
+    val hasMoreItems by librarySearchResultViewModel.hasMoreSearchItems.collectAsState()
+
+    val listState = rememberLazyListState()
+
+
+    LaunchedEffect(key1 = true) {
+        query?.let {
+            librarySearchResultViewModel.initializeSearchPager(it)
+        }
+
+    }
+
+
+    when (uiState) {
+        is UiState.Initial -> {
+            // 초기 상태 UI (예: 검색 안내 메시지)
+        }
+
+        is UiState.Loading -> {
+            LoadingIndicator()
+        }
+
+        is UiState.Success -> {
+            EndlessLazyColumn(
+                modifier = Modifier.fillMaxSize(),
+                items = searchResults,
+                headerData = null,
+                itemKey = { item: NewPipeContentListData -> item.id },
+                itemContent = { item: NewPipeContentListData ->
+                    CommonVideoItem(
+                        item = item,
+                        onClick = { mediaViewModel.setMediaItem(item as NewPipeVideoData)
+                            mainViewModel.showBottomSheet()
+
+                        })
+                },
+
+                loading = isMoreItemsLoading,
+                loadMore = { librarySearchResultViewModel.loadMoreSearchResults() },
+                hasMoreItems = hasMoreItems
+            )
+        }
+
+        is UiState.Error -> {
+            ErrorMessage(message = (uiState as UiState.Error).message)
+        }
+    }
+}
+
+
+
+@Composable
+fun ErrorMessage(message: String) {
+    // 에러 메시지 표시 구현
 }
--- app/src/main/java/com/example/transpose/ui/screen/library/search_result/LibrarySearchResultViewModel.kt
+++ app/src/main/java/com/example/transpose/ui/screen/library/search_result/LibrarySearchResultViewModel.kt
@@ -1,9 +1,109 @@
 package com.example.transpose.ui.screen.library.search_result
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.transpose.data.model.newpipe.NewPipeContentListData
+import com.example.transpose.data.repository.VideoPager
+import com.example.transpose.data.repository.newpipe.NewPipeRepository
+import com.example.transpose.ui.common.UiState
+import com.example.transpose.utils.Logger
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class LibrarySearchResultViewModel @Inject constructor(): ViewModel() {
+class LibrarySearchResultViewModel @Inject constructor(
+    private val newPipeRepository: NewPipeRepository
+): ViewModel() {
+
+    // SearchResult Screen
+    private val _searchUiState = MutableStateFlow<UiState>(UiState.Initial)
+    val searchUiState= _searchUiState.asStateFlow()
+
+    private val _searchResults = MutableStateFlow<List<NewPipeContentListData>>(emptyList())
+    val searchResults= _searchResults.asStateFlow()
+
+    private val _hasMoreSearchItems = MutableStateFlow(false)
+    val hasMoreSearchItems = _hasMoreSearchItems.asStateFlow()
+
+    private val _isMoreSearchItemsLoading = MutableStateFlow(false)
+    val isMoreItemsLoading = _isMoreSearchItemsLoading.asStateFlow()
+
+    private var searchPager: VideoPager? = null
+
+    fun initializeSearchPager(query: String) = viewModelScope.launch(Dispatchers.IO) {
+        _searchUiState.value = UiState.Loading
+        try {
+            searchPager = newPipeRepository.createSearchPager(query)
+            firstFetchSearchResult()
+        }catch (e: Exception){
+            _searchUiState.value = UiState.Error(e.toString())
+        }
+    }
+
+    private fun firstFetchSearchResult() = viewModelScope.launch(Dispatchers.IO) {
+        searchPager ?: return@launch
+        try {
+            val searchResults = newPipeRepository.fetchSearchResult(searchPager!!)
+
+            if (searchResults.isSuccess){
+                _searchResults.value = searchResults.getOrElse { emptyList() }
+                _searchUiState.value = UiState.Success
+                _hasMoreSearchItems.value = searchPager!!.isHasNextPage()
+            }
+            if (searchResults.isFailure){
+                _searchResults.value = emptyList()
+                _searchUiState.value = UiState.Error(searchResults.exceptionOrNull().toString())
+            }
+            // 다음 페이지 로직은 주석 처리된 상태로 유지
+        } catch (e: Exception) {
+            Logger.e("Error fetching search results", e)
+            _searchUiState.value = UiState.Error(e.message ?: "Unknown error occurred")
+        }
+    }
+
+    fun loadMoreSearchResults() = viewModelScope.launch(Dispatchers.IO) {
+        _isMoreSearchItemsLoading.value = false
+        searchPager ?: return@launch
+
+        try {
+            val result = newPipeRepository.fetchSearchResult(searchPager!!)
+            if (result.isSuccess){
+                _searchResults.value += result.getOrDefault(emptyList())
+                _isMoreSearchItemsLoading.value = false
+                _hasMoreSearchItems.value = searchPager!!.isHasNextPage()
+            }
+            if (result.isFailure){
+                Logger.d("loadMoreSearchResults ${result.exceptionOrNull()}")
+                _isMoreSearchItemsLoading.value = false
+            }
+        }catch (e: Exception){
+            _isMoreSearchItemsLoading.value = false
+            Logger.d("loadMoreSearchResults catch ${e}")
+        }
+    }
+
+    private val _videoUri = MutableStateFlow<String>("")
+    val videoUri = _videoUri.asStateFlow()
+
+    fun getStreamInfoByVideoId(videoId: String) = viewModelScope.launch(Dispatchers.IO){
+        try {
+            val result = newPipeRepository.fetchStreamInfoByVideoId(videoId)
+            if (result.isSuccess){
+                val bestQualityStream = result.getOrNull()?.maxByOrNull { it.getResolution() }
+                bestQualityStream?.let {
+                    Logger.d("getStreamInfoByVideoId ${it.content}")
+                    _videoUri.value = it.content
+                }
+            }
+            if (result.isFailure){
+                Logger.d("getStreamInfoByVideoId ${result.exceptionOrNull()}")
+            }
+        }catch (e: Exception){
+            Logger.d("getStreamInfoByVideoId ${e}")
+        }
+    }
 }
