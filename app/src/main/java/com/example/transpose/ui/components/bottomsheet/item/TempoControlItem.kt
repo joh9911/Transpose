85ea4dcc630da0365b680384a5482705e22df535
joh9911 <joh9911@naver.com>
2024-09-03 19:42:33 +0900

    feat: create design

--- app/src/main/java/com/example/transpose/ui/components/bottomsheet/item/TempoControlItem.kt
+++ app/src/main/java/com/example/transpose/ui/components/bottomsheet/item/TempoControlItem.kt
@@ -1,39 +1,86 @@
 package com.example.transpose.ui.components.bottomsheet.item
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import com.example.transpose.MediaViewModel
 import com.example.transpose.R
 
 
 @Composable
-fun TempoControlItem() {
-    Column(modifier = Modifier.padding(10.dp)) {
+fun TempoControlItem(mediaViewModel: MediaViewModel) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(5.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
         Text("Tempo")
-        Row {
-            IconButton(onClick = { /* Decrease tempo */ }) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 8.dp)
+                .height(48.dp)
+                .border(1.dp, Color.Gray, RoundedCornerShape(18.dp))
+                .clip(RoundedCornerShape(18.dp))
+                .background(Color.White),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+
+            Box(
+                modifier = Modifier
+                    .weight(1f)
+                    .fillMaxHeight()
+                    .clickable { mediaViewModel.tempoMinusOne() },
+                contentAlignment = Alignment.Center
+            ) {
                 Icon(
                     painter = painterResource(id = R.drawable.baseline_exposure_neg_1_24),
-                    contentDescription = "Decrease tempo"
+                    contentDescription = "Decrease Tempo",
                 )
             }
-            IconButton(onClick = { /* Reset tempo */ }) {
+            Box(
+                modifier = Modifier
+                    .weight(1f)
+                    .fillMaxHeight()
+                    .clickable { mediaViewModel.initTempoValue() },
+                contentAlignment = Alignment.Center
+            ) {
                 Icon(
                     painter = painterResource(id = R.drawable.baseline_replay_24),
-                    contentDescription = "Reset tempo"
-                )
+                    contentDescription = "Reset Tempo",
+
+                    )
             }
-            IconButton(onClick = { /* Increase tempo */ }) {
+            Box(
+                modifier = Modifier
+                    .weight(1f)
+                    .fillMaxHeight()
+                    .clickable { mediaViewModel.tempoPlusOne() },
+                contentAlignment = Alignment.Center
+            ) {
                 Icon(
                     painter = painterResource(id = R.drawable.baseline_exposure_plus_1_24),
-                    contentDescription = "Increase tempo"
+                    contentDescription = "Increase Tempo",
                 )
             }
         }
