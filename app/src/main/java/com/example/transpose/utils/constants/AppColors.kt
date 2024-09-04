4e5ac616fbde7b088ed30b0e2d2cb430033f21e2
joh9911 <joh9911@naver.com>
2024-09-04 18:02:39 +0900

    feat: add color

---
+++ app/src/main/java/com/example/transpose/utils/constants/AppColors.kt
@@ -0,0 +1,26 @@
+package com.example.transpose.utils.constants
+
+import androidx.compose.ui.graphics.Color
+
+object AppColors {
+    val Transparent = Color(0x00000000)
+    val StatusBarBackground = Color(0xFF1976D3)
+    val BlueBackground = Color(0xFF2196F3)
+    val BlueBackgroundAlpha30 = Color(0xB32196F3)
+    val BlackBackground = Color(0xFF010101)
+    val WhiteLetter = Color(0xFFFFFFFF)
+    val DrawerLine = Color(0xFFD9D9D9)
+    val DescriptionColor = Color(0xFF898989)
+    val BeforeGettingDataColor = Color(0xFFE5E5E5)
+    val PlaylistItemSelected = Color(0xFF484848)
+    val ButtonColor = Color(0xFF757575)
+    val OutsideBackground = Color(0xFFEBEBE9)
+    val BottomSheetBackground = Color(0x99000000)
+    val TransparentStatusBarBackground = Color(0x801976D3)
+    val TransparentBlackBackground = Color(0x99000000)
+    val LightGray = Color(0xFFE0E0E0)
+    val PlaceholderBlurBlue = Color(0x552196F3)
+    val DarkBlue = Color(0xFF0D47A1)
+    val GrayBackground = Color(0xFF757575)
+    val CharcoalGray = Color(0xFF3E4A59)
+}
