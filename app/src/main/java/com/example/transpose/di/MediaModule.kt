134e770b819aa1c2ca036523a1fea1a296abf683
joh9911 <joh9911@naver.com>
2024-09-05 15:06:46 +0900

    refactor: organize code

--- app/src/main/java/com/example/transpose/di/MediaModule.kt
+++ app/src/main/java/com/example/transpose/di/MediaModule.kt
@@ -6,8 +6,10 @@ import androidx.media3.common.AudioAttributes
 import androidx.media3.common.C
 import androidx.media3.common.Player
 import androidx.media3.common.util.UnstableApi
+import androidx.media3.exoplayer.DefaultLoadControl
 import androidx.media3.exoplayer.ExoPlayer
 import androidx.media3.exoplayer.trackselection.DefaultTrackSelector
+import com.example.transpose.media.CustomMediaSourceFactory
 import com.example.transpose.media.audio_effect.AudioEffectHandlerImpl
 import dagger.Module
 import dagger.Provides
@@ -27,16 +29,33 @@ class MediaModule {
         .setUsage(C.USAGE_MEDIA)
         .build()
 
+    @OptIn(UnstableApi::class)
+    @Provides
+    @Singleton
+    fun provideCustomMediaSourceFactory(@ApplicationContext context: Context): CustomMediaSourceFactory =
+        CustomMediaSourceFactory(context)
+
 
     @OptIn(UnstableApi::class)
     @Provides
     @Singleton
     fun provideExoPlayer(
         @ApplicationContext context: Context,
-        audioAttributes: AudioAttributes
+        audioAttributes: AudioAttributes,
+        customMediaSourceFactory: CustomMediaSourceFactory
     ): ExoPlayer = ExoPlayer.Builder(context)
         .setAudioAttributes(audioAttributes, true)
         .setHandleAudioBecomingNoisy(true)
+        .setLoadControl(DefaultLoadControl.Builder()
+            .setBufferDurationsMs(
+                DefaultLoadControl.DEFAULT_MIN_BUFFER_MS,
+                DefaultLoadControl.DEFAULT_MAX_BUFFER_MS,
+                DefaultLoadControl.DEFAULT_BUFFER_FOR_PLAYBACK_MS,
+                DefaultLoadControl.DEFAULT_BUFFER_FOR_PLAYBACK_AFTER_REBUFFER_MS
+            )
+            .setTargetBufferBytes(C.LENGTH_UNSET)
+            .setPrioritizeTimeOverSizeThresholds(true)
+            .build())
         .setTrackSelector(DefaultTrackSelector(context))
         .build()
 
@@ -44,25 +63,6 @@ class MediaModule {
     @Singleton
     fun providePlayer(exoPlayer: ExoPlayer): Player = exoPlayer
 
-
-//    @Provides
-//    @Singleton
-//    fun provideMediaSession(
-//        @ApplicationContext context: Context,
-//        player: Player
-//    ): MediaSession = MediaSession.Builder(context, player).build()
-
-
-//    @Provides
-//    @Singleton
-//    fun provideMusicNotificationManager(
-//        @ApplicationContext context: Context,
-//        player: Player
-//    ): MusicNotificationManager = MusicNotificationManager(context, player as ExoPlayer)
-//
-//    @Provides
-//    @Singleton
-//    fun provideMusicServiceHandler(player: Player): MusicServiceHandler = MusicServiceHandler(player as ExoPlayer)
 
     @Provides
     @Singleton
