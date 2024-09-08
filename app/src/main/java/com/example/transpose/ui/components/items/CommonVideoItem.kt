e2a3f4b930c56e10ae9467a6d28907e61c49803c
joh9911 <joh9911@naver.com>
2024-09-08 11:43:20 +0900

    feat: change Divider to HorizontalDivider

--- app/src/main/java/com/example/transpose/ui/components/items/CommonVideoItem.kt
+++ app/src/main/java/com/example/transpose/ui/components/items/CommonVideoItem.kt
@@ -12,6 +12,7 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -89,4 +90,6 @@ fun CommonVideoItem(
             )
         }
     }
+    HorizontalDivider(color = Color.LightGray, thickness = 0.5.dp)
+
 }
